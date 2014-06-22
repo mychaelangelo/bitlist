388c9e5d017e7965dcad114a7c7aab08bd8b8c96
mychaelangelo <color.ui>
2014-06-22 19:59:55 +0100

    Configured app for Production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # http://bitlist-io.herokuapp.com 
+  config.action_mailer.default_url_options = { host: 'bitlist-io.herokuapp.com'}
+
 end
