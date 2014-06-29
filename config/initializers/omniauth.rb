dc7b57545eaf58547926a4147720056ff6e267f7
mychaelangelo <color.ui>
2014-06-29 15:54:38 +0100

    Omniautho amends

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,3 @@
- Rails.application.config.middleware.use OmniAuth::Builder do
- #   provider :facebook, {:client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
-   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
-
-   on_failure { |env| SessionsController.action(:failure).call(env) }
- end
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :facebook, {:client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
+# end
