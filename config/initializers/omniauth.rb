185e70e9f76d297bc8b421303760cb8d5420297e
mychaelangelo <color.ui>
2014-06-29 14:50:59 +0100

    Omniauth facebook issue in deploy

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
-# Rails.application.config.middleware.use OmniAuth::Builder do
-#   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
-# end
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, domain: 'http://bitlist-io.herokuapp.com'
+end
